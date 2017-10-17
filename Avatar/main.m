25d8326f32d44a89ba4ec753dfb45b8ca8abea40
Anastasiia <orishchenko@i.ua>
2017-10-17 03:36:51 +0300

    Initial Commit

---
+++ Avatar/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Avatar
+//
+//  Created by Anastasia Orishchenko on 10/17/17.
+//  Copyright © 2017 Anastasiia Orishchenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
